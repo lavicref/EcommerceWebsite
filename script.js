3c65f65c5f72b2f45d22ea01f65a231df1491c73
Lavicref Nario <40537996+lavicref@users.noreply.github.com>
2019-08-14 18:24:18 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function OpenSideMenu(){
+    document.getElementById("sidemenu").style.display="block";
+    document.getElementById("open-btn").style.display="none";
+    document.getElementById("close-btn").style.display="block";
+}
+function CloseSideMenu(){
+    document.getElementById("sidemenu").style.display="none";
+    document.getElementById("open-btn").style.display="block";
+    document.getElementById("close-btn").style.display="none";
+}
